89c4cf648f1b671b31d95cebc9d3ae993da8c425
hawkeyewoman <hawkeyewoman2013@gmail.com>
2016-10-12 11:24:08 -0600

    added partials folder
    added partials folder

---
+++ lib/partials/headutils.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<title></title>
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
+   rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1"
+   crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Allan|Headland+One|Mystery+Quest" rel="stylesheet">
+<link rel="stylesheet" href="lib/css/bootstrap.css">
+<link rel="stylesheet" href="lib/css/style.css">
