8c1b59f31b2714adc2c5a0c4618a23adf57fab1e
hawkeyewoman <hawkeyewoman2013@gmail.com>
2016-10-12 11:16:53 -0600

    added bootstrap
    added bootstrap

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="lib/css/style.css">
+    <link rel="stylesheet" href="lib/css/bootstrap.css">
   </head>
   <body>
 
